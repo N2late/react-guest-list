3953b41995d598792f863e398d27ea23bf666c2c
Tiago Serra <35219940+N2late@users.noreply.github.com>
2022-09-24 20:07:28 +0200

    Update data-test-id atr

--- src/Components/GuestInfo.js
+++ src/Components/GuestInfo.js
@@ -43,7 +43,7 @@ function GuestInfo({ guest, getAllGuests }) {
   }
 
   return (
-    <div className="guestContainer" data-test-id="guest">
+    <div className="guestContainer" data-test-id={guest.id}>
       <input
         checked={checkBoxValue}
         type="checkbox"
